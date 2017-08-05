e9360b44ee0a3bf5bd752743df4d964f4a24dd41
Heike Hofmann <hofmann@iastate.edu>
2017-08-04 22:59:58 -0500

    updated help

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Examples of toolmark profiles
 #'
-#' Example data to try out the functions
+#' Simulated example data to try out the functions
 #' @format list of four profiles, pr1, pr2, pr3, and pr4. Each profile is a data frame with one variable V1. Measurements are equidistant and ordered. 
 #' pr1 and pr2 are known matches, all other pairs of profiles are known non-matches.
 #' @examples
