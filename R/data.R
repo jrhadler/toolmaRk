4803e2cc140aec237d91fb0d54df216fd93e4da7
Heike Hofmann <hofmann@iastate.edu>
2017-08-22 13:19:07 -0500

    profile description was missing

--- R/data.R
+++ R/data.R
@@ -8,6 +8,7 @@
 #'   \item{side}{A or B, indicates the side of the screwdriverused to create the toolmark. Factor variable. }
 #'   \item{angle}{degree under which the toolmark was created.}
 #'   \item{rep}{replicate number for a toolmark, side, angle composition. Note that the data here provided is not complete. For a more complete data set or more information please contact the references given below.}
+#'   \item{profile}{list of data sets with one profile each. Measurements are taken at equispaced intervals across the toolmark. .}
 #' }
 #' @references 
 #' Tool mark data included here were produced by Prof. Scott Chumbley, Mr. Stephen Davis, Ms. Taylor Grieve, Mr. Ryan Spotts, and Dr. Jeremy Hadler.  These data were produced as part of research performed at the Ames Laboratory, located on the Iowa State University campus. Ames Laboratory is operated for the U.S. Department of Energy by Iowa State University under Contract No. DE-Ac02-07CH11358.
